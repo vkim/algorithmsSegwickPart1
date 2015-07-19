4be0337e554de7dbaeb11ca867d84ddaafaaa05b
Vitaliy <vitaliy.kim@gmail.com>
2015-07-20 01:01:29 +1000

    updated

--- src/Fast.java
+++ src/Fast.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Fast {
 
@@ -46,43 +48,63 @@ public static void main(String[] args) {
 
 	private static void checkIfAnyPointsHaveEqualSlopes(Point i, Point[] tmpPoints) {
 		
-		int count=0;
+		ArrayList<Point> buff = new ArrayList<Point>();
 		
-		for(int j = 0; j<tmpPoints.length-1;j++) {
+		for(int j = 1; j<tmpPoints.length;j++) {
 			
-			if(i.slopeTo(tmpPoints[j]) == i.slopeTo(tmpPoints[j+1])) {
-				count++;
+			//look behind
+			if(i.slopeTo(tmpPoints[j-1]) == i.slopeTo(tmpPoints[j])) {
+				if(buff.isEmpty()) {
+					buff.add(tmpPoints[j-1]);
+					buff.add(tmpPoints[j]); 
+				}
+				else {
+					buff.add(tmpPoints[j]);
+				}
+				
 			}
 			else {
 				
 				//more than 3 adjacent points
-				if(count > 1 ) {
+				if(buff.size() > 2 ) {
 					
-					System.out.printf("tmpPoints = %s, j=%s, count=%s", Arrays.toString(tmpPoints), j, count);
+					buff.add(i);
 					
-					Arrays.sort(tmpPoints, j-count-1,j);
+					Collections.sort(buff);
 					
 					//if i is the first in the line
-					if(i.compareTo(tmpPoints[j-count-1])>0) {
-						printLine(j-count-1, j, tmpPoints, i);
+					if(buff.get(0) == i) {
+						printLine(buff);
 					}
 				}
 				
-				count = 0;
+				buff.clear();
 			}
 		}
 		
+		//more than 3 adjacent points
+		if(buff.size() > 2 ) {
+			
+			buff.add(i);
+			
+			Collections.sort(buff);
+			
+			//if i is the first in the line
+			if(buff.get(0) == i) {
+				printLine(buff);
+			}
+		}
 	}
 
-	private static void printLine(int i, int j, Point[] tmpPoints, Point first) {
-		
-		for(int a=i;a<j-1;a++) {
-			StdOut.print(tmpPoints[a] + " -> ");
+	private static void printLine(ArrayList<Point> line) {
+
+		for(int i = 0; i< line.size()-1;i++) {
+			StdOut.print(line.get(i) + " -> ");
 		}
 		
-		StdOut.println(tmpPoints[j-1]);
+		StdOut.println(line.get(line.size()-1));
 		
-		first.drawTo(tmpPoints[j-1]);	
+		line.get(0).drawTo(line.get(line.size()-1));	
 	}
 	
 	
